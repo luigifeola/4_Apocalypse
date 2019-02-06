4be2b3d03257be0053a036b5fe6004f46f52f48e
luigi <feola.1772197@studenti.uniroma1.it>
2019-02-06 13:24:21 +0100

    utilityPackage_WaypointCircuit.cs

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadNextScene", 3f);
+
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
